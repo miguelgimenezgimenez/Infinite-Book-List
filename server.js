59dd2a043eab84cf8e2a44c141ff0319dd1c2873
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-04-01 08:38:52 +0200

    new models commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const express = require('express')
 // const request = require('request')
-const { Book } = require('./models')
+
+const models = require('./casumo-models/models')
 
 const DIST_DIR = path.join(__dirname, 'dist')
 const PORT = process.env.port || 8080
@@ -16,6 +17,15 @@ mongoose.connect(config.mongodbUrl)
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(DIST_DIR, 'index.html'))
 // })
-Book.find({}).batchSize(10).then(console.log)
+models.Book.find({}).then(async (res) => {
+  res.forEach(async (element) => {
+    console.log(element)
+    const firstLetter = element.title[0].toUpperCase()
+    console.log(`Book${firstLetter}`)
+    const model = models[`Book${firstLetter}`]
+    console.log(model, '-----------')
+    await model.create(element)
+  })
+})
 
 app.listen(PORT, () => console.log('listening on port', PORT))
