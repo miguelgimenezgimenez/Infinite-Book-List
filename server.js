93de93d58ec9173097b1e7b5d49e8b69b91de447
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-04-01 20:17:00 +0200

    fix package jsonm

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const path = require('path')
 const express = require('express')
-const mongoose = require('mongoose')
 
 const DIST_DIR = path.join(__dirname, 'dist')
 const PORT = process.env.port || 8080
 const app = express()
-
-const config = require('./config.json')[process.env.NODE_ENV || 'development']
-
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongodbUrl)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(DIST_DIR, 'index.html'))
