a1b8158797ecf6d76c48f4f91e52643d05766641
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-04-13 17:49:54 +0200

    fix test

--- src/tests/components/ListView.spec.js
+++ src/tests/components/ListView.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import chai, { expect } from 'chai'
+import chai from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import Enzyme, { mount } from 'enzyme'
@@ -19,12 +19,8 @@ const sandbox = sinon.createSandbox()
 
 const store = createStore({})
 
-const callback = sinon.stub()
-
 describe('<ListView />', () => {
   sandbox.stub(bookActions, 'listForLetter')
-
-  const mockArray = new Array(1000)
   it('Gives the correct props to the  when rendering a  book', () => {
     // eslint-disable-next-line
     const wrapper = mount(
@@ -32,7 +28,6 @@ describe('<ListView />', () => {
         <MuiThemeProvider>
           <BrowserRouter>
             <ListView
-
               type="book"
               rowHeight={30}
               list={[]}
@@ -44,7 +39,6 @@ describe('<ListView />', () => {
         </MuiThemeProvider>
       </Provider >)
 
-    sinon.assert.calledOnce(bookActions.listForLetter)
-    // console.log(mockAction.x, mockAction.y, '--------------------------')
+    sinon.assert.calledTwice(bookActions.listForLetter)
   })
 })
